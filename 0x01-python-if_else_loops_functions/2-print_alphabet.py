d4285936435740e1368ebf3ce55b269fde724c3a
Amarajah <naomi.ibeh69@gmail.com>
2022-11-29 07:23:57 -0800

    ASCII

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Python3 program to print alphabets
+# Function to print the alphabet
+# in lower case
+def lowercaseAlphabets():
+    for c in range(97, 123):
+        print(chr(c))
