051eb28fb9b4bc94e165bcd45c6cbe63537fd8b1
Amarajah <naomi.ibeh69@gmail.com>
2022-12-04 13:18:19 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,5 +7,5 @@ def replace_in_list(my_list, idx, element):
             return(my_list)
         elif idx < 0:
             return(my_list)
-        elif idx >= len(my_list)
+        elif idx >= len(my_list) - 1:
             return(my_list)
