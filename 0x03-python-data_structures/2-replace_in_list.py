fefcf13018da9917163f6873f047245c55187959
Amarajah <naomi.ibeh69@gmail.com>
2022-12-04 13:41:41 -0800

    all

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
-    for a in my_list:
-        if idx == my_list.index(w):
-            my_list.pop(idx)
-            my_list.insert(idx, element)
-            return(my_list)
-        elif idx < 0:
-            return(my_list)
-        elif idx >= len(my_list) - 1:
-            return(my_list)
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
