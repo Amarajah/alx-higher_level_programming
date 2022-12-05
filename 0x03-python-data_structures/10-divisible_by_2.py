8c3dead656fc5d26ca8f090f7b62cc095af809ba
Amarajah <naomi.ibeh69@gmail.com>
2022-12-05 13:33:57 -0800

    go

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     copy = my_list.copy()
-    for i in my_list.copy():
-        if i % 2 == 0:
+    for i in range(len(copy)):
+        if copy[i] % 2 == 0:
             return True
         else:
             return False
