ab736e5bdcc4314b460b9270032b9c8a6a833604
Amarajah <naomi.ibeh69@gmail.com>
2022-12-08 13:23:33 -0800

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python30
 def uniq_add(my_list=[]):
     unique = []
+    result = sum(set(unique))
     for number in my_list:
         if number not in unique:
             unique.append(number)
-   result =  sum(set(unique))
    return result
