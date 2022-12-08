42fcb89fe3560ffedaeeedeba9cade6388762949
Amarajah <naomi.ibeh69@gmail.com>
2022-12-08 14:42:08 -0800

    b

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    Keymax = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
-    return Keymax
+    keymax = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
+    return keymax
