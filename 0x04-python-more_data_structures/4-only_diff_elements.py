dd1e8c2da4d4eb29737db9686b992e3bb2c1b036
Amarajah <naomi.ibeh69@gmail.com>
2022-12-08 13:51:56 -0800

    diff

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     result = []
+    res = []
     for item in set_1:
         if item not in set_2:
             result.append(item)
     return result
     for item in set_2:
         if item not in set_1:
-            result.append(item)
-    return result
+            res.append(item)
+    return res
