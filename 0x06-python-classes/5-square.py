098a5231866fed81225122e3ec77ddcca5bd79c0
Amarajah <naomi.ibeh69@gmail.com>
2022-12-20 14:57:54 -0800

    print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """ Sets the size attribute."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Returns value of size."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+
+        """Check if size is an integer."""
+
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+
+        """Check if size is less than 0."""
+
+        if value < 0:
+            raise ValueError('size must be > 0')
+
+        self._size = value
+
+    def area(self):
+        """Returns area of square."""
+        return (self._size * self._size)
+
+    def my_print(self):
+        """Prints in stdout the square with the character #."""
+
+        if self._size == 0:
+            print(" ")#print an empty line
+
+        else:
+            for i in range(self._size):
+                print("#" * self._size)
