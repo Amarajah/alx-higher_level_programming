1727fc96a0c074f7957e6627203bc9feedc199fc
Amarajah <naomi.ibeh69@gmail.com>
2022-12-21 03:32:49 -0800

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=' ')
             i += 1
-            print(" ")
-            return i
+        print(" ")
+        return i
     except IndexError:
         print(" ")
         return i
