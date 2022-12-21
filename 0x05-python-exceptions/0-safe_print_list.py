3888fdc6182c744e22178fd5b0837139a2684a22
Amarajah <naomi.ibeh69@gmail.com>
2022-12-21 03:34:19 -0800

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
 
-        for i in range(x):
-            print(my_list[i], end=' ')
+        while i < x:
+            print(my_list[i], end=" ")
             i += 1
         print(" ")
         return i
