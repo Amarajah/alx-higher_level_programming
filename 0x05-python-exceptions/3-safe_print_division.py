59e8552e420ab3d50bb57348c8c62025ada4cec0
Amarajah <naomi.ibeh69@gmail.com>
2022-12-27 13:28:51 -0800

    update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except TypeError, ZeroDivisionError:
         result = None
     finally:
-        print("{:d} / {:d} = {}".format(a, b, result))
+        print("{:d} / {:d} = {}".format(a, b, result), end=" ")
     return (result)
