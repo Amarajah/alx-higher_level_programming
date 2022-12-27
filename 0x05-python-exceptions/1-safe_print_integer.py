9e8016e77f1ec44d17934be1dc687cbf3f48fe80
Amarajah <naomi.ibeh69@gmail.com>
2022-12-27 10:32:19 -0800

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if not isinstance(value, int):
-        return False
-    else:
-        return True
-
     try:
         print("{:d}".format(int(value)), end="\n")
+        return True
     except ValueError:
         print(" ")
+        return False
