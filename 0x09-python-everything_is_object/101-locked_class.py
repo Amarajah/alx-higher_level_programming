b204ce9de648e8de992c780721f003e08482f762
Amarajah <naomi.ibeh69@gmail.com>
2023-01-03 03:44:47 -0800

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
+
 
 class LockedClass:
-
-    def __init__(self, first_name=""):
-        self.first_name = first_name
-
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError
-            ("'LockedClass' object has no attribute {}".format(name))
-        object.__setattr__(self, name, value)
+    """
+    Prevent instantiation of any any attribute except
+    first_name.
+    """
+    __slots__ = ["first_name"]
