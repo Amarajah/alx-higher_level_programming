bed12b9e4e0d6bd068e7713538c3f25f0ee301fd
Amarajah <naomi.ibeh69@gmail.com>
2023-01-02 17:32:05 -0800

    rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
 
         """ Sets the width and height attributes."""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
