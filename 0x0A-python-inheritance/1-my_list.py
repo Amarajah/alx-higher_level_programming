0a8d2cb00a44ec17da00b2be77559bee6fd4fe66
Amarajah <naomi.ibeh69@gmail.com>
2023-01-09 15:20:05 -0800

    8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,8 +7,6 @@ class MyList(list):
     '''Method to print list in ascending order.'''
 
     def print_sorted(self):
-        '''Make a copy of the list,then sort and return it.'''
-        sorted_list = self.copy
-        sorted_list.sort()
+        '''Sort and return list.'''
 
-        print(sorted_list)
+        print(sorted(self))
