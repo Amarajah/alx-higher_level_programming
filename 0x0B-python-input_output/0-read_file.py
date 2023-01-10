7ce9094feb4b9e3395e2d985be40fec59e11ed00
Amarajah <naomi.ibeh69@gmail.com>
2023-01-10 14:22:27 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     '''Opens and reads file using with statement.'''
-    with open("", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ And returns the number of characters written.
 
 def write_file(filename="", text=""):
     '''Function that writes a string to a text file using with.'''
-    with open(filename, encoding="utf-8") as f:
-        print(f.write(), end="")
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
