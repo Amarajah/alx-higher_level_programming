11b9ed98ee57472c07985429ae4b0a2108d74347
Amarajah <naomi.ibeh69@gmail.com>
2023-01-16 14:46:08 -0800

    circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unit test for rectangle class."""
+
+
+import unittest
+import io
+import sys
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import 
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for square class."""
+
+
+import unittest
+import io
+import sys
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
