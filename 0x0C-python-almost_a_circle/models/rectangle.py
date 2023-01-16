589fa5d6d701f51d91494db47f6867747a131db4
Amarajah <naomi.ibeh69@gmail.com>
2023-01-16 12:52:48 -0800

    go

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,44 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
             return ("".join(rect))
+
+    def __str__(self):
+        """ change str representantion when print object """
+        id_str = str(self.id)
+        w_str = str(self.__width)
+        h_str = str(self.__height)
+        x_str = str(self.__x)
+        y_str = str(self.__y)
+        div_str = x_str + '/' + y_str + ' - ' + w_str + '/' + h_str
+        return ("[Rectangle] " + '(' + id_str + ') ' + div_str)
+
+    def update(self, *args, **kwargs):
+        '''Update id, width, height, x and y, this order.'''
+        if args:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[2] if len(args) >= 3 else self.height
+            self.x = args[3] if len(args) >= 4 else self.x
+            self.y = args[4] if len(args) >= 5 else self.y
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x == value
+                elif key == "y":
+                    self.y == value
+    def to_dictionary(self):
+        ''' Returns dict representation of rectangle.'''
+        i = self.id
+        w = self.width
+        h = self.height
+        x = self.x
+        y = self.y
+
+        dict = {'id' : i, 'width' : w, 'height' : h, 'x' : x, 'y' : y}
+        return (dict)
