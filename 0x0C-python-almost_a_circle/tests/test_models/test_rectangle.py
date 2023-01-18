a108a92be41dd26653c302156b53dbf3546af943
Amarajah <naomi.ibeh69@gmail.com>
2023-01-18 13:09:38 -0800

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,3 +9,41 @@ import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import 
+
+
+class TestRectangle(unittest.TestCase):
+    """ class test for testing rectangle class """
+
+    def setUp(self):
+        """ reset nb_objects var to zero before each test """
+        Base._Base__nb_objects = 0
+
+    def test_isinstance(self):
+        """ check if rectangle is instance of Base and Rect """
+        r = Rectangle(1, 1)
+        self.assertIsInstance(r, Rectangle)
+        self.assertIsInstance(r, Base)
+
+    def test_private_width(self):
+        """ check if private var width is private """
+        r = Rectangle(1, 2)
+        with self.assertRaises(AttributeError):
+            r.__width
+
+    def test_private_height(self):
+        """ check if private var height is private """
+        r = Rectangle(1, 2)
+        with self.assertRaises(AttributeError):
+            r.__height
+
+    def test_private_x(self):
+        """ check if private var x is private """
+        r = Rectangle(1, 2)
+        with self.assertRaises(AttributeError):
+            r.__x
+
+    def test_private_y(self):
+        """ check if private var y is private """
+        r = Rectangle(1, 2)
+        with self.assertRaises(AttributeError):
+            r.__y
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -9,3 +9,19 @@ import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """ test class for Square class """
+
+    def setUp(self):
+        """ reset nb_objects var to zero before each test """
+        Base._Base__nb_objects = 0
+
+    def test_isinstance(self):
+        """ 10 check if square is instance of Base and Rect """
+        s = Square(1)
+        self.assertIsInstance(s, Square)
+        self.assertIsInstance(s, Rectangle)
+        self.assertIsInstance(s, Base)
+
