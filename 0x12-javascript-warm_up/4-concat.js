ffaada3393e6ef5897f0bf9de7afbd26473d4b40
Amarajah <naomi.ibeh69@gmail.com>
2023-03-13 14:31:19 -0700

    4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,5 +4,5 @@ if (process.argv.length === 2) {
 } else if (process.argv.length === 3) {
     console.log(process.argv[2] + ' is undefined');
 } else {
-    console.log(process.argv[2] + ' is' + process.argv[3]);
+    console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
