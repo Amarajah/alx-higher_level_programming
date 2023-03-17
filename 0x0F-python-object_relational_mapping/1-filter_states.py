6c85f322b11e225962fa873a32da4cbd76276ae4
Amarajah <naomi.ibeh69@gmail.com>
2023-03-17 05:35:18 -0700

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module States
+script that lists all states with capital N
+from the database hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    MY_USER = argv[1]
+    MY_PASSWD = argv[2]
+    MY_DB = argv[3]
+
+    '''Open Database connection'''
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=MY_USER,
+                           passwd=MY_PASSWD,
+                           db=MY_DB)
+
+    '''Create cursor for operate over DB'''
+    myCursor = mydb.cursor()
+
+    '''pass and execute a SQL sentence'''
+    myCursor.execute("select * from states \
+                     WHERE name LIKE 'N%'\
+                     ORDER BY id ASC")
+
+    '''retrive records and fill cursor'''
+    states = myCursor.fetchall()
+
+    '''Print rows'''
+    for state in states:
+        print(state)
+
+    '''close cursor'''
+    myCursor.close()
+
+    '''close connection to DB'''
+    mydb.close()
