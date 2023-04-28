9d3cdc989cbb172b1cb3b3b644ee66466d0bfc76
Amarajah <naomi.ibeh69@gmail.com>
2023-04-28 15:15:09 -0700

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# manage urllib.error.HTTPError exceptions and
+# print: Error code: followed by the HTTP status code
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as output:
+            print(output.read().decode('UTF-8'))
+    except error.HTTPError as err:
+        print('Error code:', err.code)
