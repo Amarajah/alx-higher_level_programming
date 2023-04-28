dd8c1e64f053174930f3543749abc95980c8e9ce
Amarajah <naomi.ibeh69@gmail.com>
2023-04-28 16:05:16 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+'''
+import sys
+import requests
+
+if __name__ == '__main__':
+    request = post(argv[1], {'email': argv[2]})
+    print(request.text)
