2da97e8d594c537b81a8aad821623e08b5eb854b
Amarajah <naomi.ibeh69@gmail.com>
2023-04-28 17:08:02 -0700

    github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+'''
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+        pwd = sys.argv[2]
+
+    r = request.get(url, auth=(user, pwd))
+    json = r.json()
+    print("{}".format(json.get('id')))
